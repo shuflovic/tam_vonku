f0abefba82a3141716c5365f4b24868a4bc38610
shuflovic <pavel.skuta@gmail.com>
2025-12-24 09:33:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -254,7 +254,7 @@ async function fetchAndDisplayFlightDetails() {
       </table>
     </div>
     
-<button class="detail-button" a href="visual.html" target="_blank">Flights on World map</button>
+<button class="detail-button" onclick="window.open('visual.html', '_blank')">Flights on World map</button>
     
   `;
 }
